6844badeb05f52b4ab0a3614d2ec32f12520c115
Adam Levitt <adam.levitt@gmail.com>
2022-06-04 01:55:40 -0400

    Arrange code into function for readibility

--- sudoku_webscrape.py
+++ sudoku_webscrape.py
@@ -3,9 +3,34 @@ import requests
 
 
 def request_sudoku():
-    '''Get response from website'''
+    """Get response from website"""
     response = requests.get("https://www.sudokuweb.org/").text
     return response
+
+
+def bf_soup(take):
+    """Parse data using BeautifulSoup"""
+
+    puzzle_get = []
+
+    # Loop and get text/class data to fill in Suduo puzzle
+    for count in range(81):
+
+        if count == 0:
+            specify = "right"
+        else:
+            specify = "right" + str(count)
+
+        square_class = take.find(id=specify).span["class"][0]
+        square_number = take.find(id=specify).span.text
+
+        if square_class == "sedy":
+            puzzle_get.append(int(square_number))
+
+        else:
+            puzzle_get.append(0)
+
+    return puzzle_get
 
 
 if __name__ == "__main__":
@@ -15,30 +40,9 @@ if __name__ == "__main__":
     with open("web_response.html", 'w') as file:
         file.write(response)
 
-    puzzle_get = []
-
-    #Open file in context manager
+    # Open file in context manager
     with open("web_response.html", "r") as file:
         soup = BeautifulSoup(file.read(), "lxml")
 
-        #Loop and get text/class data to fill in Suduo puzzle
-        for count in range(81):
-
-            if count == 0:
-                specify = 'right'
-            else:
-                specify = 'right' + str(count)
-            
-            
-            square = soup.find(id=specify).span
-            square_class = soup.find(id=specify).span['class'][0]
-            square_number = soup.find(id=specify).span.text
-        
-            if square_class == 'sedy':
-                puzzle_get.append(int(square_number))
-
-            else:
-                puzzle_get.append(0)
-
-        
-    print(puzzle_get)
+    puzzle = bf_soup(soup)
+    print(puzzle)
