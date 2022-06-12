cc3f7c7824d53044bc9113480bfc080a6ef0dcb2
Adam Levitt <adam.levitt@gmail.com>
2022-06-12 00:22:16 -0400

    Display Squares for Number Controls

--- sudoku_game.py
+++ sudoku_game.py
@@ -1,5 +1,4 @@
 import pygame
-from regex import D
 import sudoku_solve
 import sudoku_webscrape
 import sys
@@ -14,6 +13,8 @@ LEFT_GUTTER = 3
 RIGHT_GUTTER = 1
 TOP_GUTTTER = 1
 BOTTOM_GUTTER = 3
+NUMBERS_INDENT = 2
+NUMBERS_GAP = 10
 
 WIDTH, HEIGHT = BLOCK_SIZE * (GRID_SIZE + LEFT_GUTTER + RIGHT_GUTTER), BLOCK_SIZE * (GRID_SIZE + TOP_GUTTTER + BOTTOM_GUTTER)
 FPS = 60
@@ -48,8 +49,26 @@ class display_board:
 
     def highlight_boardsquare(self):
         """Highligh a cell if clicked"""
+
         if self.highlight == "Y":
             pygame.draw.rect(WINDOW, BRIGHT_GREEN, self.rect_clicked, 3)
+
+    def display_number_controls(self):
+        """Display Number Selections"""
+
+        left_limit = BLOCK_SIZE * (LEFT_GUTTER + NUMBERS_INDENT)
+        right_limit = WIDTH - (BLOCK_SIZE * (RIGHT_GUTTER + NUMBERS_INDENT))
+        nblock_size = int(((right_limit - left_limit) - (4 * NUMBERS_GAP)) / 5)
+        top_limit = int(
+            (BLOCK_SIZE * (TOP_GUTTTER + GRID_SIZE))
+            + ((HEIGHT - (BLOCK_SIZE * (TOP_GUTTTER + GRID_SIZE))) - ((2 * nblock_size) + NUMBERS_GAP)) / 2
+        )
+        bottom_limit = top_limit + 2 * nblock_size + NUMBERS_GAP
+
+        for across in range(left_limit, right_limit, nblock_size + NUMBERS_GAP):
+            for down in range(top_limit, bottom_limit, nblock_size + NUMBERS_GAP):
+                rectangle = pygame.Rect(across, down, nblock_size, nblock_size)
+                pygame.draw.rect(WINDOW, WHITE, rectangle, 2)
 
 
 def main():
@@ -93,6 +112,7 @@ def main():
                     break
 
         board.display_board_main()
+        board.display_number_controls()
         board.highlight_boardsquare()
         pygame.display.update()
 
